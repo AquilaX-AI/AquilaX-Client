fbdb199a21307abd03a4a1182c127865b8fbb533
Omer Khan <mdomerkhan8000@gmail.com>
2025-05-16 16:05:21 +0530

    update

--- aquilax/index.py
+++ aquilax/index.py
@@ -99,7 +99,7 @@ def save_config(config):
 
 def get_version():
     try:
-        version = "1.2.8"
+        version = "1.2.9"
         return version
     except Exception as e:
         logger.error(f"Failed to get the version")
